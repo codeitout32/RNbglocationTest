2ef0d39f9f9f5584436c1ff2cdb5f87808b3e9fb
Homendra Chandrawanshi <symentix.homendra@gmail.com>
2022-02-18 21:31:49 +0530

    fixed time format in drops listing

--- packages/web/src/components/dropsListing/table.tsx
+++ packages/web/src/components/dropsListing/table.tsx
@@ -254,7 +254,7 @@ export default function DropTable() {
                         {/* Conditional social links ends */}
                       </TableCell>
                       <TableCell align="left">
-                        {moment(row.launch_date).utc().format("HH:mm A")} UTC{" "}
+                        {moment(row.launch_date).utc().format("hh:mm A")} UTC{" "}
                         {print(row.launch_date)}
                       </TableCell>
                       <TableCell align="left">{row.supply_content}</TableCell>
