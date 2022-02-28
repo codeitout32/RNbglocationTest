ad8c995525a8ef1e12e48e6de7f164d153093b24
SymentixRahul <symentix.rahul@gmail.com>
2022-02-28 13:57:09 +0530

    remove dummy image and add new testimonial images

--- packages/web/src/components/services/testimonials/index.tsx
+++ packages/web/src/components/services/testimonials/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Avatar,
   Box,
@@ -7,45 +8,45 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
-import { title } from "process";
-import React from "react";
-import StyledGradPaper from "./gradPaper";
 import Marquee from "react-fast-marquee";
+import { title } from "process";
+
+import StyledGradPaper from "./gradPaper";
 
 const Testimonials = () => {
   const tList = [
     {
-      image: "https://picsum.photos/65",
+      image: "images/testimonials/IMG_5173.PNG",
       title: "Frost",
       content:
         "The team has been constant with updates and continuously showed us proof of their work which shows they are here for the long term. ",
     },
     {
-      image: "https://picsum.photos/65",
+      image: "images/testimonials/IMG_5174.PNG",
       title: "The Banished",
       content:
         " What they have been building so far is what the space needs, trust and amazing product. We are not just investing our money but our trust in them. Believing is the key.",
     },
     {
-      image: "https://picsum.photos/65",
+      image: "images/testimonials/IMG_5175.PNG",
       title: "Deserter",
       content:
         "This is not just a basic NFT but a huge pot with great potential for passive income. In Solana space, 70% are trying to flip but we are here with complete dedication and patience.",
     },
     {
-      image: "https://picsum.photos/65",
+      image: "images/testimonials/IMG_5176.PNG",
       title: "Mad Eyes",
       content:
         "The project I’m looking at is called Nova Nerds, it’s a supply of 1111 nerds. The alpha is that they are building a marketplace called “NFT hub” it’s a combination of a Marketplace, drops, news, and services. They will have improved UI, fast listings, and free PR for collections that choose to go exclusive with them.",
     },
     {
-      image: "https://picsum.photos/65",
+      image: "images/testimonials/IMG_5177.PNG",
       title: "Renegade ",
       content:
         "I have been a fan of NN since the start as they will be providing everything a project needs such as DAO verification, the upcoming market place which could be a really big play in my opinion. They are providing multiple services such as doxxing and Twitter discord bots at such a low cost as compared to other service providers, it's gonna be huge.",
     },
     {
-      image: "https://picsum.photos/65",
+      image: "images/testimonials/IMG_5173.PNG",
       title: "Voodoo ",
       content:
         "You'll gain royalties from trade on secondary if you hold a Nova Nerd (the more you own, the more royalties you get - 50% is shared with holders) + with the NFT Hub team is building you'll get another source of passive income (25%) + you will gain access to our private holder section where we share alpha about upcoming drops, and play games together. What more do you expect!",
