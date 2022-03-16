b25edcb9231881eb76738ec8d4a7bbc118d18ab4
SymentixRahul <symentix.rahul@gmail.com>
2022-03-16 10:14:06 +0530

    update news slice

--- packages/common/slices/news.slice.ts
+++ packages/common/slices/news.slice.ts
@@ -1,57 +1,34 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import formatISO from "date-fns/formatISO";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import formatISO from 'date-fns/formatISO';
 
 export interface NewsList {
   newsList: Array<Object>;
+  newNewsList: Array<Object>;
   loading: boolean;
-  singleNews: object;
-
-  recentNews: {
-    recentNewsList: Array<Object>;
-    pagination: Object;
-  };
+  newNewsLoading: boolean;
   success: string;
-  lastRefreshTime: string;
+  lastRefreshTime: string | null;
   error: object | string;
-  featuredNews: object | string;
-  relatedNews: {
-    relatedNewsList: {
-      rows: Array<object>;
-    };
-    pagination: object;
-  };
 }
 
 const initialState: NewsList = {
   newsList: [{}],
-  singleNews: {},
-  relatedNews: {
-    relatedNewsList: {
-      rows: [],
-    },
-    pagination: {},
-  },
-  recentNews: {
-    recentNewsList: [],
-    pagination: {},
-  },
-  featuredNews: {
-    rows: [],
-  },
-  lastRefreshTime: "",
+  newNewsList: [{}],
+  lastRefreshTime: null,
   loading: false,
-  success: "",
-  error: "",
+  newNewsLoading: false,
+  success: '',
+  error: ''
 };
 
 export const newsSlice = createSlice({
-  name: "newsList",
+  name: 'newsList',
   initialState,
   reducers: {
     fetchNewsStart: (state, action) => {
       return {
         ...state,
-        loading: true,
+        loading: true
       };
     },
     fetchNewsSuccess: (state, action) => {
@@ -59,129 +36,68 @@ export const newsSlice = createSlice({
         ...state,
         loading: false,
         newsList: action.payload,
-        lastRefreshTime: new Date().toISOString(),
+        lastRefreshTime: new Date().toISOString()
       };
+    },
+    updateNewsStateToRead: (state, action) => {
+      const { readNewsId, isRead } = action.payload;
+      console.log("🚀~ readNewsId, isRead", readNewsId, isRead)
+      state.newsList.res.rows =  state.newsList.res.rows.map((news: object) => {
+        if (news.id === readNewsId) {
+          return { ...news, isRead };
+        }
+        return news;
+      });
     },
     fetchNewsError: (state, action: PayloadAction<object>) => {
       return {
         ...state,
         loading: false,
-        error: action.payload,
+        error: action.payload
       };
     },
-    fetchSingleNewsStart: (state) => {
+    fetchNewNewsStart: (state, action) => {
       return {
         ...state,
-        loading: true,
+        newNewsLoading: true
       };
     },
-    fetchSingleNewsSuccess: (state, action) => {
+    fetchNewNewsSuccess: (state, action) => {
+      const newRows = action.payload?.res?.rows;
+      const oldNews = state.newsList?.res?.rows;
+      const newNews = {
+        res: {
+          rows: [...newRows, ...oldNews],
+          count: state.newsList.count + action.payload.count
+        }
+      };
+
       return {
         ...state,
-        loading: false,
-        singleNews: action.payload,
-        success: "success",
+        newNewsLoading: false,
+        newNewsList: action.payload,
+        newsList: newNews,
+        lastRefreshTime: new Date().toISOString()
       };
     },
-    fetchSingleNewsError: (state, action: PayloadAction<object>) => {
+    fetchNewNewsError: (state, action: PayloadAction<object>) => {
       return {
         ...state,
-        loading: false,
-        error: action.payload,
+        newNewsLoading: false,
+        error: action.payload
       };
-    },
-    fetchRelatedNewsStart: (state, action: PayloadAction<object>) => {
-      return {
-        ...state,
-        loading: true,
-      };
-    },
-    fetchRelatedNewsSuccess: (state, action: PayloadAction<object>) => {
-      return {
-        ...state,
-        loading: false,
-        relatedNews: {
-          ...state.relatedNews,
-          relatedNewsList: action.payload,
-        },
-      };
-    },
-    fetchRelatedNewsError: (state, action: PayloadAction<object>) => {
-      return {
-        ...state,
-        loading: false,
-        error: action.payload,
-      };
-    },
-    fetchRecentNewsStart: (state, action: PayloadAction<object>) => {
-      return {
-        ...state,
-        loading: true,
-        recentNews: {
-          ...state.recentNews,
-          pagination: {
-            ...action.payload,
-          },
-        },
-      };
-    },
-    fetchRecentNewsSuccess: (state, action: PayloadAction<Array<object>>) => {
-      return {
-        ...state,
-        loading: false,
-        recentNews: {
-          ...state.recentNews,
-          recentNewsList: action.payload,
-        },
-      };
-    },
-    fetchRecentNewsError: (state, action: PayloadAction<object>) => {
-      return {
-        ...state,
-        loading: false,
-        error: action.payload,
-      };
-    },
-    fetchFeaturedNewsStart: (state, action: PayloadAction<object>) => {
-      return {
-        ...state,
-        loading: true,
-      };
-    },
-    fetchFeaturedNewsSuccess: (state, action: PayloadAction<object>) => {
-      return {
-        ...state,
-        loading: false,
-        featuredNews: action.payload,
-      };
-    },
-    fetchFeaturedNewsError: (state, action: PayloadAction<object>) => {
-      return {
-        ...state,
-        loading: false,
-        error: action.payload,
-      };
-    },
-  },
+    }
+  }
 });
 
-// Action creators are generated for each case reducer function
 export const {
   fetchNewsStart,
   fetchNewsSuccess,
   fetchNewsError,
-  fetchSingleNewsStart,
-  fetchSingleNewsSuccess,
-  fetchSingleNewsError,
-  fetchRelatedNewsStart,
-  fetchRelatedNewsSuccess,
-  fetchRelatedNewsError,
-  fetchRecentNewsStart,
-  fetchRecentNewsSuccess,
-  fetchRecentNewsError,
-  fetchFeaturedNewsStart,
-  fetchFeaturedNewsSuccess,
-  fetchFeaturedNewsError,
+  fetchNewNewsStart,
+  fetchNewNewsSuccess,
+  fetchNewNewsError,
+  updateNewsStateToRead
 } = newsSlice.actions;
 
 export default newsSlice.reducer;
