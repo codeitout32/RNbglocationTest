b5ad33334863838212f6dab9cee9de7c10c6b487
SymentixRahul <symentix.rahul@gmail.com>
2022-03-16 10:13:17 +0530

    update news selector

--- packages/common/selectors/news.selector.ts
+++ packages/common/selectors/news.selector.ts
@@ -1,37 +1,45 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { AppState } from "../store";
+import { createSelector } from '@reduxjs/toolkit';
+import { AppState } from '../store';
 
 const newsReducer = (state: AppState) => state.news;
 
 export const newsListSelector = createSelector(
   newsReducer,
-  (news) => news.newsList
+  news => news.newsList??[]
+);
+export const newNewsListSelector = createSelector(
+  newsReducer,
+  news => news.newNewsList
 );
 export const singleNewsSelector = createSelector(
   newsReducer,
-  (news) => news.singleNews
+  news => news.singleNews
 );
 export const newsLoadingSelector = createSelector(
   newsReducer,
-  (news) => news.loading
+  news => news.loading
+);
+export const newNewsLoadingSelector = createSelector(
+  newsReducer,
+  news => news.newNewsLoading
 );
 export const featuredNewsSelector = createSelector(
   newsReducer,
-  (news) => news.featuredNews
+  news => news.featuredNews
 );
 export const recentNewsSelector = createSelector(
   newsReducer,
-  (news) => news.recentNews
+  news => news.recentNews
 );
 export const relatedNewsSelector = createSelector(
   newsReducer,
-  (news) => news.relatedNews
+  news => news.relatedNews
 );
 export const recentNewsPaginationSelector = createSelector(
   newsReducer,
-  (news) => news.recentNews.pagination
+  news => news.recentNews.pagination
 );
 export const lastRefreshTimeSelector = createSelector(
   newsReducer,
-  (news) => news.lastRefreshTime
+  news => news.lastRefreshTime
 );
