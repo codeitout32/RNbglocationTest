dddf41d1c063f839ad5cb293609a0d543a1be4cd
SymentixRahul <symentix.rahul@gmail.com>
2022-03-17 19:38:46 +0530

    update header

--- packages/mainApp/src/components/Header/index.tsx
+++ packages/mainApp/src/components/Header/index.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Linking,
-  StyleProp,
-  TextStyle,
-  ViewStyle,
-  Pressable,
-} from 'react-native';
-import {Header as HeaderRNE, HeaderProps, Icon} from 'react-native-elements';
-import {TouchableOpacity} from 'react-native-gesture-handler';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {StyleSheet, View, Linking, Pressable} from 'react-native';
+import {Header as HeaderRNE, Icon} from 'react-native-elements';
 
 type HeaderComponentProps = {
   title: string;
@@ -21,54 +10,57 @@ type HeaderComponentProps = {
   noSettings: boolean;
 };
 
-type ParamList = {
-  Detail: {
-    openDrawer: void;
-  };
-};
-
-// navigation.navigate('Home', {catId: item.id});
-
 const Header: React.FunctionComponent<HeaderComponentProps> = props => {
   const docsNavigate = () => {
     console.log('hello');
     Linking.openURL(`https://reactnativeelements.com/docs/${props.view}`);
   };
   const headerLinks = props.headerLinks;
-  const playgroundNavigate = () => {
-    console.log('hello');
-    // Linking.openURL(`https://react-native-elements.js.org/#/${props.view}`);
-  };
   const menuNavigate = () => {
     props.navigation.navigate(headerLinks.menu.link, headerLinks.menu.params);
   };
   const settingsNavigate = () => {
     props.navigation.navigate('Settings', headerLinks.menu.params);
   };
-  console.log('hello from header');
+
+  const handleRefresh = () => {
+    props.navigation.navigate(headerLinks.relaod.link, {isReload: true});
+  };
   return (
     <HeaderRNE
       containerStyle={styles.headerContainer}
       leftComponent={
-        <Pressable onPress={menuNavigate}>
-          <Icon
-            name={headerLinks.menu.icon.name}
-            type={headerLinks.menu.icon.type}
-            size={30}
-            color="white"
-          />
-        </Pressable>
+        <>
+          <Pressable onPress={menuNavigate}>
+            <Icon
+              name={headerLinks.menu.icon.name}
+              type={headerLinks.menu.icon.type}
+              size={30}
+              color='white'
+            />
+          </Pressable>
+        </>
       }
       rightComponent={
         <View style={styles.headerRight}>
           {props.noSettings ? (
             <Pressable onPress={() => props.navigation.goBack()}>
-              <Icon name="back" color="white" type="antdesign" />
+              <Icon name='back' color='white' type='antdesign' />
             </Pressable>
           ) : (
-            <Pressable onPress={settingsNavigate}>
-              <Icon name="settings" color="white" />
-            </Pressable>
+            <>
+              <Pressable style={styles.refreshIcon} onPress={handleRefresh}>
+                <Icon
+                  name={headerLinks.menu.reloadIcon.name}
+                  type={headerLinks.menu.reloadIcon.type}
+                  size={26}
+                  color='white'
+                />
+              </Pressable>
+              <Pressable onPress={settingsNavigate}>
+                <Icon name='settings' color='white' />
+              </Pressable>
+            </>
           )}
         </View>
       }
@@ -86,9 +78,7 @@ const styles = StyleSheet.create({
     flexShrink: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // height: 50,
     backgroundColor: '#34CF54',
-    // marginBottom: 20,
     width: '100%',
     paddingVertical: 15,
   },
@@ -106,6 +96,9 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  refreshIcon: {
+    marginRight: 16,
   },
 });
 
